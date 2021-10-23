7d6c8310142cdb1087b6362ca887e3fd4d63af8e
rotemasud <rotemasud@gmail.com>
2021-10-23 21:17:58 +0300

    increase the min of the auto scalling policy to 2

--- ecs/main.tf
+++ ecs/main.tf
@@ -173,7 +173,7 @@ resource "aws_ecs_service" "main" {
 
 resource "aws_appautoscaling_target" "ecs_target" {
   max_capacity       = 4
-  min_capacity       = 1
+  min_capacity       = 2
   resource_id        = "service/${aws_ecs_cluster.main.name}/${aws_ecs_service.main.name}"
   scalable_dimension = "ecs:service:DesiredCount"
   service_namespace  = "ecs"
